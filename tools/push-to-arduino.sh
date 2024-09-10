04316b97cdf4d1f93f979aa5e42bbe885eca1b3e
HamzaHajeir <hahajeir@gmail.com>
2024-09-10 21:07:50 +0300

    Updates and enhances scripts

--- tools/push-to-arduino.sh
+++ tools/push-to-arduino.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
-source ./tools/install-arduino.sh
+source ./tools/config.sh
 
 if [ -x $GITHUB_TOKEN ]; then
 	echo "ERROR: GITHUB_TOKEN was not defined"
+	exit 1
+fi
+
+if ! [ -d "$AR_COMPS/arduino" ]; then
+	echo "ERROR: Target arduino folder does not exist!"
 	exit 1
 fi
 
@@ -20,18 +24,19 @@ git config --global user.email "$GITHUB_ACTOR@github.com"
 # esp32-arduino-libs
 #
 
-LIBS_ZIP_FILENAME="esp32-arduino-libs-$LIBS_VERSION.zip"
-LIBS_JSON_FILENAME="package-$LIBS_VERSION.json"
-IDF_LIBS_ZIP_URL="https://github.com/$AR_LIBS_REPO/releases/download/$LIBS_RELEASE_TAG/$LIBS_ZIP_FILENAME"
-IDF_LIBS_JSON_URL="https://github.com/$AR_LIBS_REPO/releases/download/$LIBS_RELEASE_TAG/$LIBS_JSON_FILENAME"
-
-if [ $AR_HAS_COMMIT == "0" ] || [ $LIBS_HAS_ASSET == "0" ]; then
+if [ $LIBS_HAS_COMMIT == "0" ] || [ $AR_HAS_COMMIT == "0" ]; then
 	cd "$AR_ROOT"
-	mkdir -p dist
-
-	# check if the release exists
-	if [ $LIBS_HAS_RELEASE == "0" ]; then
-		echo "Release for tag \"$LIBS_RELEASE_TAG\" not found. Please create the release first."
+	# create branch if necessary
+	if [ "$LIBS_HAS_BRANCH" == "1" ]; then
+		echo "Branch '$AR_NEW_BRANCH_NAME' Already Exists"
+		echo "Switching to esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'..."
+		git -C "$IDF_LIBS_DIR" checkout $AR_NEW_BRANCH_NAME
+	else
+		echo "Creating esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'..."
+		git -C "$IDF_LIBS_DIR" checkout -b $AR_NEW_BRANCH_NAME
+	fi
+	if [ $? -ne 0 ]; then
+		echo "ERROR: Checkout of branch '$AR_NEW_BRANCH_NAME' failed"
 		exit 1
 	fi
 
@@ -70,47 +75,34 @@ if [ $AR_HAS_COMMIT == "0" ] || [ $LIBS_HAS_ASSET == "0" ]; then
 
 	echo "Downloading asset '$LIBS_ZIP_FILENAME' and checking integrity..."
 
-	# Download the file
-	remote_file="remote-$LIBS_ZIP_FILENAME"
-	curl -s -L -o "$remote_file" "$IDF_LIBS_ZIP_URL"
+		# Download the file
+		filename="esp32-arduino-libs-$IDF_LIBS_COMMIT.zip"
+		curl -s -o "$filename" "$IDF_LIBS_DL_URL"
 
-	# Check if the download was successful
-	if [ $? -ne 0 ]; then
-		echo "Error downloading file from $IDF_LIBS_ZIP_URL"
-		exit 1
-	fi
-
-	# Calculate the remote file checksum and size
-	remote_checksum=$(sha256sum "$remote_file" | awk '{print $1}')
-	remote_size=$(stat -c%s "$remote_file")
-
-	echo "Local: $local_size bytes, $local_checksum"
-	echo "Remote: $remote_size bytes, $remote_checksum"
-
-	# Check if the checksums match
-	if [ "$local_checksum" != "$remote_checksum" ]; then
-		echo "Checksum mismatch for downloaded file"
-		echo "Deleting asset and exiting..."
-		if [ `github_release_asset_delete "$AR_LIBS_REPO" "$LIBS_ASSET_ID"` == "0" ]; then
-			echo "ERROR: Failed to delete asset '$LIBS_ZIP_FILENAME'"
+		# Check if the download was successful
+		if [ $? -ne 0 ]; then
+		  echo "Error downloading file from $IDF_LIBS_DL_URL"
+		  exit 1
 		fi
-		exit 1
-	fi
 
-	# Clean up the downloaded file
-	rm "$remote_file"
+		# Calculate the size in bytes and SHA-256 sum
+		size=$(stat -c%s "$filename")
+		sha256sum=$(sha256sum "$filename" | awk '{print $1}')
 
-	# Print the results
-	echo "Tool: esp32-arduino-libs"
-	echo "Version: $LIBS_VERSION"
-	echo "URL: $IDF_LIBS_ZIP_URL"
-	echo "File: $LIBS_ZIP_FILENAME"
-	echo "Size: $local_size bytes"
-	echo "SHA-256: $local_checksum"
-	echo "JSON: $AR_OUT/package_esp32_index.template.json"
-	cd "$AR_ROOT"
-	python3 tools/add_sdk_json.py -j "$AR_OUT/package_esp32_index.template.json" -n "esp32-arduino-libs" -v "$LIBS_VERSION" -u "$IDF_LIBS_ZIP_URL" -f "$LIBS_ZIP_FILENAME" -s "$local_size" -c "$local_checksum"
-	if [ $? -ne 0 ]; then exit 1; fi
+		# Clean up the downloaded file
+		rm "$filename"
+
+		# Print the results
+		echo "Tool: esp32-arduino-libs"
+		echo "Version: $LIBS_VERSION"
+		echo "URL: $IDF_LIBS_DL_URL"
+		echo "File: $filename"
+		echo "Size: $size bytes"
+		echo "SHA-256: $sha256sum"
+		echo "JSON: $AR_OUT/package_esp32_index.template.json"
+		cd "$AR_ROOT"
+		python3 tools/add_sdk_json.py -j "$AR_OUT/package_esp32_index.template.json" -n "esp32-arduino-libs" -v "$LIBS_VERSION" -u "$IDF_LIBS_DL_URL" -f "$filename" -s "$size" -c "$sha256sum"
+		if [ $? -ne 0 ]; then exit 1; fi
 
 	JSON_ASSET_ID=`github_release_asset_upload "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "$LIBS_JSON_FILENAME" "$AR_OUT/package_esp32_index.template.json"`
 	if [ -z "$JSON_ASSET_ID" ]; then
@@ -127,7 +119,7 @@ fi
 # esp32-arduino
 #
 
-if [ $AR_HAS_COMMIT == "0" ] || [ $LIBS_HAS_ASSET == "0" ]; then
+if [ $AR_HAS_COMMIT == "0" ]; then
 	cd "$AR_ROOT"
 	# create or checkout the branch
 	if [ ! $AR_HAS_BRANCH == "0" ]; then
@@ -138,31 +130,31 @@ if [ $AR_HAS_COMMIT == "0" ] || [ $LIBS_HAS_ASSET == "0" ]; then
 		git -C "$AR_COMPS/arduino" checkout -b $AR_NEW_BRANCH_NAME
 	fi
 	if [ $? -ne 0 ]; then
-		echo "ERROR: Checkout of branch '$AR_NEW_BRANCH_NAME' failed"
+	    echo "ERROR: Checkout of branch '$AR_NEW_BRANCH_NAME' failed"
 		exit 1
 	fi
 
 	# make changes to the files
 	echo "Patching files in branch '$AR_NEW_BRANCH_NAME'..."
 	rm -rf "$AR_COMPS/arduino/package/package_esp32_index.template.json" && cp -f "$AR_OUT/package_esp32_index.template.json" "$AR_COMPS/arduino/package/package_esp32_index.template.json"
-
+	
 	cd $AR_COMPS/arduino
 
 	# did any of the files change?
 	if [ -n "$(git status --porcelain)" ]; then
 		echo "Pushing changes to branch '$AR_NEW_BRANCH_NAME'..."
-		git add . && git commit --message "$AR_NEW_COMMIT_MESSAGE" && git push -u origin $AR_NEW_BRANCH_NAME
+	    git add . && git commit --message "$AR_NEW_COMMIT_MESSAGE" && git push -u origin $AR_NEW_BRANCH_NAME
 		if [ $? -ne 0 ]; then
-			echo "ERROR: Pushing to branch '$AR_NEW_BRANCH_NAME' failed"
+		    echo "ERROR: Pushing to branch '$AR_NEW_BRANCH_NAME' failed"
 			exit 1
 		fi
 	else
-		echo "No changes in branch '$AR_NEW_BRANCH_NAME'"
-		if [ $AR_HAS_BRANCH == "0" ]; then
-			echo "Delete created branch '$AR_NEW_BRANCH_NAME'"
-			git branch -d $AR_NEW_BRANCH_NAME
-		fi
-		exit 0
+	    echo "No changes in branch '$AR_NEW_BRANCH_NAME'"
+	    if [ $AR_HAS_BRANCH == "0" ]; then
+	    	echo "Delete created branch '$AR_NEW_BRANCH_NAME'"
+	    	git branch -d $AR_NEW_BRANCH_NAME
+	    fi
+	    exit 0
 	fi
 
 	# CREATE PULL REQUEST
