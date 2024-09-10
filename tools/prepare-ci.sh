d1853504260313287d45ad3a869678849cad4aea
HamzaHajeir <hahajeir@gmail.com>
2024-09-10 21:07:50 +0300

    Updates and enhances scripts

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
-pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
+# Ubuntu setup
+# Change in archive-build.sh gawk to awk
+#sudo apt update && sudo apt install -y gperf cmake ninja-build
+#pip3 install wheel future pyelftools
+
+# MacOS (ARM) setup
+# Change in archive-build.sh awk to gawk
+brew install gsed
+brew install gawk
+brew install gperf
+brew install ninja
+brew install ccache
+python -m pip install --upgrade pip
+pip install wheel future pyelftools
