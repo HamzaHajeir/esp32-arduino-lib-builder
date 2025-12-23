c96c807c84c63d906fe3669b74d3920ee518c1e6
Hamza Hajeir <hahajeir@gmail.com>
2025-12-23 22:34:59 +0300

    Supporting the old workflow

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -3,3 +3,9 @@
 sudo apt update && sudo apt install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
 pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
+
+brew install gsed
+brew install gawk
+brew install gperf
+brew install ninja
+brew install ccache
