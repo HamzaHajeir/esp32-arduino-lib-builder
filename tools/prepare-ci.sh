4223b30eac9db42d6c013707a96715845bbbbf7c
Hamza Hajeir <hahajeir@gmail.com>
2025-12-23 22:47:30 +0300

    Installs required tools for only the mocos-operated workflows

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -4,8 +4,20 @@ sudo apt update && sudo apt install -y git wget curl libssl-dev libncurses-dev f
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
 pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
 
-brew install gsed
-brew install gawk
-brew install gperf
-brew install ninja
-brew install ccache
+# Check if the system is macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "Detected macOS. Checking Homebrew installation..."
+
+	# Install Homebrew if it is not already installed
+	if ! command -v brew &> /dev/null; then
+		echo "Homebrew not found. Installing..."
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+	brew install gsed
+	brew install gawk
+	brew install gperf
+	brew install ninja
+	brew install ccache
+else
+	echo "This script is meant for macOS. Current OS: $OSTYPE."
+fi
